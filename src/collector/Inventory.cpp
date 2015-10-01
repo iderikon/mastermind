0ef979958fc81b54a128c0492234962693244204
Vladimir Nikulichev <saturnus@yandex-team.ru>
2015-10-01 22:23:23 +0300

    redesign inventory API

--- src/collector/Inventory.cpp
+++ src/collector/Inventory.cpp
@@ -24,12 +24,36 @@
 Inventory::Inventory()
     :
     m_handle(nullptr),
-    m_inventory_error(nullptr),
-    m_get_dc_by_host(nullptr)
+    m_driver(new DefaultInventory)
 {}
+
+Inventory::~Inventory()
+{
+    m_driver.reset();
+    if (m_handle != nullptr)
+        dlclose(m_handle);
+}
+
+std::string DefaultInventory::get_dc_by_host(const std::string & addr, std::string & error_text)
+{
+    if (!addr.empty()) {
+        BH_LOG(app::logger(), DNET_LOG_DEBUG,
+                "Default inventory: Returning DC '%s' for host '%s'",
+                addr.c_str(), addr.c_str());
+    } else {
+        error_text = "Default inventory: Get DC: addr is empty";
+    }
+    return addr;
+}
 
 int Inventory::open_shared_library(const std::string & file_name)
 {
+    if (m_handle != nullptr) {
+        BH_LOG(app::logger(), DNET_LOG_ERROR,
+                "Internal error: Inventory is already opened");
+        return -1;
+    }
+
     m_handle = dlopen(file_name.c_str(), RTLD_LAZY | RTLD_LOCAL);
 
     if (m_handle == nullptr) {
@@ -40,33 +64,51 @@ int Inventory::open_shared_library(const std::string & file_name)
         return -1;
     }
 
-    if ((m_inventory_error = (const char* (*)()) dlsym(m_handle, "inventory_error")) == nullptr ||
-            (m_get_dc_by_host = (int (*)(const char*, char*, size_t)) dlsym(m_handle, "get_dc_by_host")) == nullptr) {
+    InventoryDriver *(*create_inventory)() = (InventoryDriver *(*)()) dlsym(m_handle, "create_inventory");
+    if (create_inventory == nullptr) {
         const char *err = dlerror();
         BH_LOG(app::logger(), DNET_LOG_ERROR,
-                "Inventory: dlsym() failed for '%s': %s",
+                "Inventory: Cannot find symbol 'create_inventory' in '%s': '%s'",
                 file_name.c_str(), (err != nullptr ? err : "unknown error"));
-
-        m_inventory_error = nullptr;
-        m_get_dc_by_host = nullptr;
-        dlclose(m_handle);
+        close();
         return -1;
     }
+
+    InventoryDriver *driver = create_inventory();
+    if (driver == nullptr) {
+        BH_LOG(app::logger(), DNET_LOG_ERROR, "Inventory: inventory_create() returned null");
+        close();
+        return -1;
+    }
+
+    m_driver.reset(driver);
 
     return 0;
 }
 
+void Inventory::close()
+{
+    if (m_handle != nullptr) {
+        m_driver.reset(new DefaultInventory);
+        dlclose(m_handle);
+        m_handle = nullptr;
+    }
+}
+
 std::string Inventory::get_dc_by_host(const std::string & addr)
 {
-    if (m_get_dc_by_host != nullptr && m_inventory_error != nullptr) {
-        char buf[256];
-        if (m_get_dc_by_host(addr.c_str(), buf, sizeof(buf)) != 0) {
-            BH_LOG(app::logger(), DNET_LOG_ERROR,
-                    "Inventory: get_dc_by_host(%s) failed: %s",
-                    addr.c_str(), m_inventory_error());
-            return std::string();
-        }
-        return std::string(buf);
+    std::string error_text;
+    std::string result = m_driver->get_dc_by_host(addr, error_text);
+
+    if (result.empty()) {
+        BH_LOG(app::logger(), DNET_LOG_ERROR,
+                "Inventory: Cannot resolve DC for host '%s': %s",
+                addr.c_str(), error_text.c_str());
+    } else {
+        BH_LOG(app::logger(), DNET_LOG_DEBUG,
+                "Inventory: Resolved DC '%s' for host '%s'",
+                result.c_str(), addr.c_str());
     }
-    return addr;
+
+    return result;
 }
--- src/collector/Inventory.h
+++ src/collector/Inventory.h
@@ -19,21 +19,39 @@
 #ifndef __bbd6c304_2898_4ea0_afae_ee8b16e21893
 #define __bbd6c304_2898_4ea0_afae_ee8b16e21893
 
+#include <memory>
 #include <string>
+
+class InventoryDriver
+{
+public:
+    // This method is not guaranteed to be thread-safe.
+    virtual std::string get_dc_by_host(const std::string & addr, std::string & error_text) = 0;
+};
+
+class DefaultInventory : public InventoryDriver
+{
+public:
+    virtual std::string get_dc_by_host(const std::string & addr, std::string & error_text);
+};
 
 class Inventory
 {
 public:
     Inventory();
+    ~Inventory();
 
+    // Open shared object file containing C-linkage function
+    // create_inventory(void) returning InventoryDriver*
     int open_shared_library(const std::string & file_name);
+
+    void close();
 
     std::string get_dc_by_host(const std::string & addr);
 
 private:
     void *m_handle;
-    const char *(*m_inventory_error)(void);
-    int (*m_get_dc_by_host)(const char *, char *, size_t);
+    std::unique_ptr<InventoryDriver> m_driver;
 };
 
 #endif
